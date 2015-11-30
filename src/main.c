20ae0b30e371b2690fbe3bfb634b47df0941e4ac
GermanSC <germanscillone@gmail.com>
2015-11-30 17:59:12 -0300

    Commit Inicial

---
+++ src/main.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Project	 : Server_Con1
+ Author      : German Sc.
+ Version     : 0.0
+ Copyright   : Completamente copyrighteado 2015
+ Description : Nada.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char *argv[])
+{
+	printf("Test Cliente\n");
+
+	return 0;
+}
