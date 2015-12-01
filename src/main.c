17f20b050b6a072afd5ced6a16d3ce6b2237e7b6
GermanSC <germanscillone@gmail.com>
2015-12-01 13:16:00 -0300

    Implementada comunicacion TCP/IP

--- src/main.c
+++ src/main.c
@@ -11,37 +11,77 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define MAX_CMD	20
 
 int main(int argc, char *argv[])
 {
+	/*	Variables de conexión	*/
 	char	ip[]	=	"127.0.0.1";
 	int		port	=	15001;
+	int		sockfd	=	-1;
+	struct sockaddr_in server_info;
+
+	/*	Variables de control	*/
 	int 	i		=	0;
-	char   *argls[10]= {NULL};
+	int		ctrl = 0;
+
+	/*	Variables de datos	*/
+	char   *argls[MAX_CMD]= {NULL};
+	char test[6];
+
+	/*	Comienzo del programa	*/
 
 	if (argc != 2)
 	{
 		printf("uso: cliente \"comando [opciones] [argumentos]\" \n Comillas necesarias.\n");
 		return -1;
 	}
-	printf("Envando comando: \"%s\" a servidor remoto %s:%d\n\n",argv[1],ip,port);
+	printf("Envando comando: \"%s\" a servidor remoto %s:%d\n",argv[1],ip,port);
 
 	/*Genero lista de comandos a partir del argumento principal	*/
 
 	argls[0] = strtok(argv[1], " ");
 	while (argls[i] != NULL)
 	{
-	    printf ("%s\n",argls[i]);
-	    i++;;
+	    i++;
 	    argls[i] = strtok (NULL, " \n");
 	}
 
+	/*	Socket Set-up	*/
+	sockfd = socket(PF_INET,SOCK_STREAM,0);
+		if(sockfd == -1)
+		{
+			printf(">>ERROR: No se pudo abrir el socket.\n\n");
+			return -1;
+		}
 
+	inet_pton(AF_INET, ip, &(server_info.sin_addr));
+	server_info.sin_family		=	AF_INET;
+	server_info.sin_port		=	htons(port);
 
+	printf(">Intentando conectar con servidor...\n");
+
+	ctrl = connect (sockfd, (struct sockaddr *)&server_info, sizeof (struct sockaddr_in));
+	if(ctrl == -1)
+		{
+			printf(">>ERROR: No se pudo establecer la conexión.\n\n");
+			close(sockfd);
+			return -1;
+		}
+
+	printf("Conexión exitosa.\n");
+
+	read(sockfd,test,6);
+	printf("%s\n",test);
+
+	close(sockfd);
+	/*	Fin de Programa	*/
 	return 0;
 }
